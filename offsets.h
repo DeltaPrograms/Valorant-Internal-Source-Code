75114381a7d1aeb22fff3b189e8b720877fa720f
FIX <cloudmek7@gmail.com>
2023-02-16 02:40:05 +0700

    Add project files.

---
+++ offsets.h
@@ -0,0 +1,11 @@
+#pragma once
+namespace Offsets
+{
+	constexpr auto
+		Key = 0x9005AB8,
+		State = 0x9005A80,
+		ProcessEvent = 0x3321FF0,
+		StaticFindObject = 0x3360200,
+		TriggerVEH = 0x1B6D880,
+		GetActorBounds = 0x507d240;
+}
